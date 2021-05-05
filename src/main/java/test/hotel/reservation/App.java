e8a19e158d6232857676d5172f345e44ddfdb473
GayatriNanoti <40693221+GayatriNanoti@users.noreply.github.com>
2021-05-05 18:40:17 +0530

    Update App.java

--- src/main/java/test/hotel/reservation/App.java
+++ src/main/java/test/hotel/reservation/App.java
@@ -1,1 +1,15 @@
 
+package test.hotel.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    	System.out.println("hello");
+    }
+}
